bf3150f9ff9e1d15612d19a85abfc6cd31c5cd54
jmgeo <j.mills.geo@gmail.com>
2021-08-01 21:17:50 -0500

    Coding along. Working the json reading.

--- static/js/app.js
+++ static/js/app.js
@@ -3,5 +3,15 @@ function getPlots(id) {
     //Use the d3 to read the json
     d3.json("samples.json").then (sampledata =>{
         console.log(sampledata)
+
+        var otuIds = sampledata.samples[0].otu_ids;
+        console.log(otuIds)
+
+        var sampleValues =  sampledata.samples[0].sample_values.slice(0,10).reverse();
+        console.log(sampleValues)
+
+        var otuLabels =  sampledata.samples[0].otu_labels.slice(0,10);
+        console.log (otuLabels)
+
     })
 }
